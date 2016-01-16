493d4db4675489cdd4df62cc200ad7a7c5451d6c
Rogelio Segovia <segovro@gmail.com>
2016-01-16 15:29:38 -0500

    Update cc.js

--- js/cc.js
+++ js/cc.js
@@ -5,46 +5,35 @@ var from = web3.eth.coinbase;
 web3.eth.defaultAccount = from;
 
 var communitycurrencyContract = web3.eth.contract([
-	{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"type":"function"},
-	{"constant":false,"inputs":[{"name":"_authorized","type":"address"}],"name":"accessMyWallet","outputs":[],"type":"function"},
-	{"constant":true,"inputs":[{"name":"_monitored","type":"address"}],"name":"monitorWallet","outputs":[{"name":"_getCCUs","type":"int256"},
-	{"name":"_getCredit","type":"uint256"},
-	{"name":"_getDeadline","type":"uint256"},
-	{"name":"_getMoneyLender","type":"address"},
-	{"name":"_getUnitsOfTrust","type":"uint256"},
-	{"name":"_getIsMember","type":"bool"},
-	{"name":"_getReputation","type":"uint256"},
-	{"name":"_getLast","type":"uint256"},
-	{"name":"_getGdpActivity","type":"uint256"}],"type":"function"},
-	{"constant":false,"inputs":[{"name":"_beneficiary","type":"address"},
-	{"name":"_createReputation","type":"uint256"}],"name":"mintAssignReputation","outputs":[],"type":"function"},
-	{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"type":"function"},
-	{"constant":false,"inputs":[{"name":"_newMember","type":"address"}],"name":"acceptMember","outputs":[],"type":"function"},
-	{"constant":false,"inputs":[{"name":"_newCommunity","type":"address"}],"name":"newCommunity","outputs":[],"type":"function"},
-	{"constant":false,"inputs":[{"name":"_beneficiary","type":"address"},
-	{"name":"_createCCUs","type":"int256"}],"name":"mintAssignCCUs","outputs":[],"type":"function"},
-	{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"type":"function"},
-	{"constant":false,"inputs":[{"name":"_newTreasury","type":"address"}],"name":"newTreasury","outputs":[],"type":"function"},
-	{"constant":false,"inputs":[{"name":"_payee","type":"address"},
-	{"name":"_payment","type":"uint256"}],"name":"transfer","outputs":[],"type":"function"},
-	{"constant":false,"inputs":[{"name":"_newVatRate","type":"int256"},
-	{"name":"_newRewardRate","type":"uint256"},
-	{"name":"_newIniCCUs","type":"int256"},
-	{"name":"_newIniR","type":"uint256"}],"name":"newParameters","outputs":[],"type":"function"},
-	{"constant":false,"inputs":[{"name":"_oldMember","type":"address"}],"name":"kickOutMember","outputs":[],"type":"function"},
-	{"constant":false,"inputs":[{"name":"_borrower","type":"address"},
-	{"name":"_credit","type":"uint256"},
-	{"name":"_blocks","type":"uint256"}],"name":"credit","outputs":[],"type":"function"},
-	{"inputs":[],"type":"constructor"},
-	{"anonymous":false,"inputs":[{"indexed":false,"name":"_payment","type":"uint256"},
-	{"indexed":false,"name":"_myBalanceCCUs","type":"int256"},
-	{"indexed":true,"name":"_to","type":"address"}],"name":"Transfer","type":"event"},
-	{"anonymous":false,"inputs":[{"indexed":false,"name":"_credit","type":"uint256"},
-	{"indexed":false,"name":"_blocks","type":"uint256"},
-	{"indexed":false,"name":"_myunitsOfTrust","type":"uint256"},
-	{"indexed":false,"name":"_myReputationBalance","type":"uint256"},
-	{"indexed":true,"name":"_borrower","type":"address"}],"name":"Credit","type":"event"}
+{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"type":"function"},
+{"constant":false,"inputs":[{"name":"_authorized","type":"address"}],"name":"accessMyWallet","outputs":[],"type":"function"},
+{"constant":true,"inputs":[{"name":"_monitored","type":"address"}],"name":"monitorWallet","outputs":[{"name":"_getCCUs","type":"int256"},
+{"name":"_getCredit","type":"uint256"},{"name":"_getDeadline","type":"uint256"},{"name":"_getMoneyLender","type":"address"},
+{"name":"_getUnitsOfTrust","type":"uint256"},{"name":"_getIsMember","type":"bool"},{"name":"_getAlias","type":"string"},
+{"name":"_getReputation","type":"uint256"},{"name":"_getLast","type":"uint256"},{"name":"_getGdpActivity","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"_beneficiary","type":"address"},
+{"name":"_createReputation","type":"uint256"}],"name":"mintAssignReputation","outputs":[],"type":"function"},
+{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"type":"function"},
+{"constant":false,"inputs":[{"name":"_createCCUs","type":"int256"}],"name":"mintCCUs","outputs":[],"type":"function"},
+{"constant":false,"inputs":[{"name":"_newCommunity","type":"address"}],"name":"newCommunity","outputs":[],"type":"function"},
+{"constant":false,"inputs":[{"name":"_newMember","type":"address"},
+{"name":"_newAlias","type":"string"}],"name":"acceptMember","outputs":[],"type":"function"},
+{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"type":"function"},
+{"constant":false,"inputs":[{"name":"_newTreasury","type":"address"}],"name":"newTreasury","outputs":[],"type":"function"},
+{"constant":false,"inputs":[{"name":"_payee","type":"address"},{"name":"_payment","type":"uint256"}],"name":"transfer","outputs":[],"type":"function"},
+{"constant":false,"inputs":[{"name":"_newDemurrage","type":"int256"},{"name":"_newrewardRate","type":"uint256"},
+{"name":"_newIniCCUs","type":"int256"},{"name":"_newIniR","type":"uint256"}],"name":"newParameters","outputs":[],"type":"function"},
+{"constant":false,"inputs":[{"name":"_oldMember","type":"address"}],"name":"kickOutMember","outputs":[],"type":"function"},
+{"constant":false,"inputs":[{"name":"_borrower","type":"address"},
+{"name":"_credit","type":"uint256"},{"name":"_daysAfter","type":"uint256"}],"name":"credit","outputs":[],"type":"function"},
+{"inputs":[],"type":"constructor"},
+{"anonymous":false,"inputs":[{"indexed":false,"name":"_payment","type":"uint256"},
+{"indexed":false,"name":"_myBalanceCCUs","type":"int256"},{"indexed":true,"name":"_to","type":"address"}],"name":"Transfer","type":"event"},
+{"anonymous":false,"inputs":[{"indexed":false,"name":"_credit","type":"uint256"},{"indexed":false,"name":"_blocks","type":"uint256"},
+{"indexed":false,"name":"_myunitsOfTrust","type":"uint256"},{"indexed":false,"name":"_myReputationBalance","type":"uint256"},
+{"indexed":true,"name":"_borrower","type":"address"}],"name":"Credit","type":"event"}
 ]);
+
+var communitycurrency = communitycurrencyContract.new()
 
 var communityCurrency = communitycurrencyContract.at(address);
 
@@ -81,5 +70,3 @@ function credit()  {}
 function monitorWallet() {}
 
 function accessMyWallet () {}
-
-

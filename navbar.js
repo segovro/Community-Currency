11e16cc6f1a8b4086f041c433d0f425ea46eb146
segovro <segovro@gmail.com>
2016-03-06 21:06:04 -0500

    lateral navbar

--- navbar.js
+++ navbar.js
@@ -1,52 +1,33 @@
 document.write(
-		'<nav class="navbar navbar-inverse">'+
-		'  <div class="container-fluid">'+
-		'    <div class="navbar-header">'+
-		'      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">'+
-		'        <span class="icon-bar"></span>'+
-		'        <span class="icon-bar"></span>'+
-		'        <span class="icon-bar"></span> '+
-		'      </button>'+
-		'    <a class="navbar-brand" href="https://github.com/segovro/Community-Currency/wiki" target="blank"><span class="glyphicon glyphicon-hourglass"></span> Community Monetary System </a>'+
-		'    </div>'+
-		'    <div class="collapse navbar-collapse" id="myNavbar">'+
-		'      <ul class="nav navbar-nav">'+
-		'      <li><a href="index.html"><span class="glyphicon glyphicon-home"></span> Home</a></li>'+
-		'      <li class="dropdown-submenu">'+
-		'          <a class="dropdown-toggle" data-toggle="dropdown" href="payment.html"><span class="glyphicon glyphicon-transfer"></span> Transactions</a>'+
-		'          <ul class="dropdown-menu">'+
-		'          <li><a href="payment.html"><span class="glyphicon glyphicon-send"></span> Payment</a></li> '+
-		'          <li><a href="transactions.html"><span class="glyphicon glyphicon-transfer"></span> Transactions</a></li>        '+
-		'          <li><a href="exchange.html"><span class="glyphicon glyphicon-random"></span> Exchange</a></li>        '+
-		'          </ul>'+
-		'        </li>'+
-		'        <li class="dropdown-submenu">'+
-		'          <a class="dropdown-toggle" data-toggle="dropdown" href="endorseCredit.html"><span class="glyphicon glyphicon-thumbs-up"></span> P2P Credits</a>'+
-		'          <ul class="dropdown-menu">'+
-		'        <li><a href="endorseCredit.html"><span class="glyphicon glyphicon-ok"></span> Endorse P2P Credit</a></li> '+
-		'        <li><a href="access.html"><span class="glyphicon glyphicon-log-in"></span> Give access</a></li> '+
-		'        <li><a href="monitor.html"><span class="glyphicon glyphicon-eye-open"></span> Monitor account</a></li> '+
-		'          </ul>'+
-		'        </li>'+
-		'        <li class="dropdown-submenu">'+
-		'          <a class="dropdown-toggle" data-toggle="dropdown" href="civilService.html"><span class="glyphicon glyphicon-user"></span> Commoner</a>'+
-		'          <ul class="dropdown-menu">'+
-		'          <li><a href="civilService.html"><span class="glyphicon glyphicon-hourglass"></span> Community Service</a></li>        '+
-		'          <li><a href="funding.html"><span class="glyphicon glyphicon-cloud-upload"></span> Community Funding</a></li>        '+
-		'          </ul>'+
-		'        </li>'+
-		'        <li class="dropdown-submenu">'+
-		'          <a class="dropdown-toggle" data-toggle="dropdown" href="budget.html"><span class="glyphicon glyphicon-cloud"></span> Community</a>'+
-		'          <ul class="dropdown-menu">'+
-		'        <li><a href="commonersList.html"><span class="glyphicon glyphicon-user"></span> Commoners List</a></li> '+
-		'        <li><a href="budget.html"><span class="glyphicon glyphicon-th-list"></span> Commons Finances</a></li> '+
-		'        <li><a href="commune.html"><span class="glyphicon glyphicon-home"></span> Commune</a></li> '+
-		'        <li><a href="treasury.html"><span class="glyphicon glyphicon-lock"></span> Treasury</a></li>'+
-		'        <li><a href="administration.html"><span class="glyphicon glyphicon-inbox"></span> Administration</a></li>'+
-		'          </ul>'+
-		'        </li>'+
-		'      </ul>'+
-		'    </div>'+
-		'  </div>'+
-		'</nav>'
+
+		'      <ul class=""list-group"">'+
+		
+		'      <li class="list-group-item"><a href="https://github.com/segovro/Community-Currency/wiki"><span class="glyphicon glyphicon-hourglass"></span><b> CC Wiki</b></a></li>'+
+		
+		'      <li class="list-group-item"><a href="index.html"><span class="glyphicon glyphicon-home"></span><b> My Account</b></a></li>'+
+		
+		'      <li class="list-group-item"><span class="glyphicon glyphicon-transfer"></span><b> Transactions</b></li>'+
+		'          <li class="list-group-item"><a href="payment.html"><span class="glyphicon glyphicon-send"></span> Payment</a></li> '+
+		'          <li class="list-group-item"><a href="transactions.html"><span class="glyphicon glyphicon-transfer"></span> Transactions</a></li>        '+
+		'          <li class="list-group-item"><a href="exchange.html"><span class="glyphicon glyphicon-random"></span> Exchange</a></li>        '+
+  
+		'      <li class="list-group-item"><span class="glyphicon glyphicon-thumbs-up"></span><b> P2P Credits</b></li>'+
+		'        <li class="list-group-item"><a href="endorseCredit.html"><span class="glyphicon glyphicon-ok"></span> Endorse P2P Credit</a></li> '+
+		'        <li class="list-group-item"><a href="access.html"><span class="glyphicon glyphicon-log-in"></span> Give access</a></li> '+
+		'        <li class="list-group-item"><a href="monitor.html"><span class="glyphicon glyphicon-eye-open"></span> Monitor account</a></li> '+
+
+
+		'      <li class="list-group-item"><span class="glyphicon glyphicon-user"></span><b> Commoner</b></li>'+
+		'          <li class="list-group-item"><a href="civilService.html"><span class="glyphicon glyphicon-hourglass"></span> Community Service</a></li>        '+
+		'          <li class="list-group-item"><a href="funding.html"><span class="glyphicon glyphicon-cloud-upload"></span> Community Funding</a></li>        '+
+
+		'      <li class="list-group-item"><span class="glyphicon glyphicon-cloud"></span><b> Community</b></li>'+
+		'        <li class="list-group-item"><a href="commonersList.html"><span class="glyphicon glyphicon-user"></span> Commoners List</a></li> '+
+		'        <li class="list-group-item"><a href="budget.html"><span class="glyphicon glyphicon-th-list"></span> Commons Finances</a></li> '+
+		'        <li class="list-group-item"><a href="commune.html"><span class="glyphicon glyphicon-home"></span> Commune</a></li> '+
+		'        <li class="list-group-item"><a href="treasury.html"><span class="glyphicon glyphicon-lock"></span> Treasury</a></li>'+
+		'        <li class="list-group-item"><a href="administration.html"><span class="glyphicon glyphicon-inbox"></span> Administration</a></li>'+
+
+		'      </ul>'
+
 );
